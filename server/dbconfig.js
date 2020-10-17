5daa068cb7ae0f3b3979a36103c0dd142425fdcb
doveriko <abelmorpe@gmail.com>
2020-10-17 19:32:03 +0200

    add server project and .gitignore

---
+++ server/dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+    user :'foo',
+    password :'foo',
+    server:'127.0.0.1',
+    database:'Products',
+    options:{
+        trustedconnection: true,
+        enableArithAbort : true, 
+        instancename :'SQLEXPRESS'
+    },
+    port : 55892
+}
+
+module.exports = config; 
