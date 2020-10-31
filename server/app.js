6b8bbe35f5c8644eee3784ffb4be9beb1290995c
doveriko <abelmorpe@gmail.com>
2020-10-31 18:02:47 +0100

    create API & config db operations

--- server/app.js
+++ server/app.js
@@ -1,25 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const ticketsRouter = require('./routes/tickets');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Router middleware
+app.use('/tickets', ticketsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var TYPES = require('tedious').TYPES;
+var db = require('../dboperations');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
